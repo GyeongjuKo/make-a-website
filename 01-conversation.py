d928b93c0f0f604ed9f961e196d6519637c23b74
GyeongjuKo <matiakkj@gmail.com>
2017-01-10 00:49:43 +0900

    02-data-type-1.py

---
+++ 01-conversation.py
@@ -0,0 +1,7 @@
+# print(1 +2)
+#
+name = input("What is your Name : ")
+print("Hello, ", name)
+name = input("What is your Name : ")
+print("Hello, "+name)
+# print("Hel"+"lo")
---
+++ 02-data-type-1.py
@@ -0,0 +1,13 @@
+# integer
+print (1+1)
+
+#float
+print(2.5+3.4)
+
+#string
+print("hello")
+
+print(float(2))
+print("1")
+print(int("1"))
+print(int("1.5"))
